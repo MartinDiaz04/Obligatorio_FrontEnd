edc37b7d1b6e115079f38bf515465d4ee86eadf1
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-04 20:04:17 -0300

    chore: Refactor Registro component to use Bootstrap classes for styling

--- src/componentes/Registro.js
+++ src/componentes/Registro.js
@@ -65,18 +65,21 @@ const Registro = () => {
   };
 
   return (
-    <div>
-      <label htmlFor="usuario">Ingrese usuario</label>
-      <input type="text" name="usuario" ref={usuario}></input>
-      <label htmlFor="pass">Ingrese clave</label>
-      <input type="password" name="pass" ref={pass}></input>
-      <label htmlFor="departamento">Seleccione departamento:</label>
-      <select onChange={obtenerCiudades} ref={departamentoActual} name="departamentos">{departamentos.map(departamento => (<option key={departamento.id} value={departamento.id}>{departamento.nombre}
-      </option>))}
-      </select>
-      <label htmlFor="ciudades">Seleccione ciudad:</label>
-      <select name="ciudades" ref={ciudad}>{ciudades.map((ciudad) => (<option key={ciudad.id} value={ciudad.id}> {ciudad.nombre}</option>))}</select>
-      <input type="button" value="Registrar" onClick={registrarUsuario} />
+    <div className="container-fluid">
+      <div className="row d-flex justify-content-center align-items-center">
+        <label className="text-center mt-3" htmlFor="usuario">Ingrese usuario</label>
+        <input className="col-6 p-2" type="text" name="usuario" ref={usuario}></input>
+        <label className="text-center mt-3" htmlFor="pass">Ingrese clave</label>
+        <input className="col-6 p-2" type="password" name="pass" ref={pass}></input>
+        <label className="text-center mt-3" htmlFor="departamento">Seleccione departamento:</label>
+        <select className="col-6 text-center p-2" onChange={obtenerCiudades} ref={departamentoActual} name="departamentos">{departamentos.map(departamento => (<option key={departamento.id} value={departamento.id}>{departamento.nombre}
+        </option>))}
+        </select>
+        <label className="text-center mt-3" htmlFor="ciudades">Seleccione ciudad:</label>
+        <select className="col-6 text-center p-2" name="ciudades" ref={ciudad}>{ciudades.map((ciudad) => (<option key={ciudad.id} value={ciudad.id}> {ciudad.nombre}</option>))}</select>
+        <label htmlFor="boton"></label>
+        <input className="mt-3 col-1 p-2" name="boton" type="button" value="Registrar" onClick={registrarUsuario} />
+      </div>
     </div>
   );
 };
