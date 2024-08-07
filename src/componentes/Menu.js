c4b69aa517a4ba365af7fa88e7ed80c52e554a57
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-07 17:34:37 -0300

    feat: Update Menu component with branding and styling

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -1,14 +1,19 @@
-import { Outlet } from "react-router-dom"
+
 import { Link } from "react-router-dom"
 
 
 const Menu = () => {
-  return (
-    <div id="menu">
-        <Link className="boton" to="registro">Registrarse</Link>
-        <Link className="boton" to="login">Iniciar Sesion</Link>
-    </div>
-  )
+    return (
+        <div>
+            <div className="d-flex justify-content-center my-5">
+                <h1 className="">Baby Tracker</h1>
+            </div>
+            <div className="d-flex justify-content-center">
+                <Link className="boton mx-5" to="registro">Registrarse</Link>
+                <Link className="boton mx-5" to="login">Iniciar Sesion</Link>
+            </div>
+        </div>
+    )
 }
 
 export default Menu
