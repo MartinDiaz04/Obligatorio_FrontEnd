e5436242a9f9b37e4596d8c41bb0a2434434bb8b
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-08 21:15:57 -0300

    feat: Add NavBar component to enable navigation in the dashboard

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Registro from './componentes/Registro';
 import Login from './componentes/Login';
 import Menu from './componentes/Menu';
 import Dashboard from './componentes/Dashboard';
+import NavBar from './componentes/NavBar';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -12,10 +13,12 @@ function App() {
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Menu />}/>
+          <Route path="/" element={<Menu />} />
           <Route path="/registro" element={<Registro />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<NavBar />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </Provider>
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom"
+const CerrarSesion = () => {
+  return (
+    <div>
+      <h1>Cerrar Sesion</h1>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default CerrarSesion
