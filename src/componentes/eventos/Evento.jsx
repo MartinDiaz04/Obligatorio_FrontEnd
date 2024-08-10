e88e0824e3964b8d97cbb74ce30ef323cde44fc7
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-09 21:57:42 -0300

    refactor: Optimize Evento component by lazy loading images and improving code formatting

--- src/componentes/eventos/Evento.jsx
+++ src/componentes/eventos/Evento.jsx
@@ -4,29 +4,15 @@ import { useSelector } from 'react-redux'
 const Evento = ({ detalle, idCategoria, fecha }) => {
     const url = "https://babytracker.develotion.com/imgs/"
     const fechaReducida = fecha.split(' ')[0]
-    const categorias = useSelector(state => state.categoria.listaCategorias)
-    console.log(idCategoria)
+    const categorias = useSelector(state => state.categoria.listaCategorias)    
     const img = categorias.filter(categoria => categoria.id == idCategoria)[0].imagen
+    const urlImagen = url + img + ".png"
     
-    useEffect(() =>{
-        fetch(url + img + ".png", {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json',
-        }
-        })
-        .then((r) => r.json())
-        .then((data) => {
-            console.log(data)
-    })
-    })
-    
-
     return (
         <div className="row d-flex justify-content-center align-items-center">
-            <h1>{detalle}</h1>
-            <p>{idCategoria}</p>
+            <img src={urlImagen} alt="imagen" />                        
             <p>{fechaReducida}</p>
+            <h1>{detalle}</h1>
         </div>
     )
 }
