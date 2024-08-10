290cffe3e158f779f110104edd597943e1e99c92
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-10 17:03:22 -0300

    refactor: Update Menu component UI and improve code structure

--- src/componentes/Menu.jsx
+++ src/componentes/Menu.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom"
 
 const Menu = () => {
     return (
-        <div>
-            <div className="d-flex justify-content-center my-5">
-                <h1 className="">Baby Tracker</h1>
+        <div className="menu-container">
+            <div className="d-flex justify-content-center  my-5">
+                <h1 className="menu-title">Baby Tracker</h1>
             </div>
             <div className="d-flex justify-content-center">
-                <Link className="boton mx-5 text-center" to="registro">Registrarse</Link>
-                <Link className="boton mx-5 text-center" to="login">Iniciar Sesion</Link>
+                <Link className="btn btn-secondary mx-3 d-flex justify-content-center align-items-center" to="registro">Registrarse</Link>
+                <Link className="btn btn-secondary mx-3 d-flex justify-content-center align-items-center" to="login">Iniciar Sesion</Link>
             </div>
         </div>
     )
