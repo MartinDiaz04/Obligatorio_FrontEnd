de782aad4a8b4b9fa98800c9e3a520c4489bd1d2
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-10 14:46:55 -0300

    refactor: Optimize code formatting and lazy load images in ListarEventosHoy component

--- src/componentes/eventos/ListarEventosHoy.jsx
+++ src/componentes/eventos/ListarEventosHoy.jsx
@@ -5,18 +5,13 @@ import Evento from "./Evento"
 const ListarEventos = () => {
   const eventos = useSelector(state => state.evento.listaEventos)
   const hoy = new Date()
-  const año = hoy.getFullYear()
-  const mes = String(hoy.getMonth() + 1).padStart(2, '0'); // padStart agrega un 0 si el numero es menor a 10 para poder comparar con la fecha recibida por el evento
-  const dia = String(hoy.getDate()).padStart(2, '0');
-  const fecha = `${año}-${mes}-${dia}`
-  
   return (
     <div className="row d-flex justify-content-center listaEventos">
       
         <h1 className="text-center">Eventos del dia de hoy</h1>
         {/* Le saco la hora para comprar solo el dia */}
-        {eventos.filter(e => e.fecha.split(' ')[0] == fecha).map((e, i) => (
-          <Evento key={i} {...e} />
+        {eventos.filter(e => e.fecha.split(' ')[0] == hoy.toISOString().split('T')[0]).map(e => (
+          <Evento key={e.id} {...e} />
         ))
         }
 
