d7c8b703c8f376356eb98e06f780942f9e58a250
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-10 15:17:30 -0300

    refactor: Fix typo in BiberonesConsumidos component and improve code formatting

--- src/componentes/eventos/BiberonesConsumidos.jsx
+++ src/componentes/eventos/BiberonesConsumidos.jsx
@@ -3,16 +3,13 @@ import { useState, useEffect } from "react"
 
 const BiberonesConsumidos = () => {
     const eventos = useSelector(state => state.evento.listaEventos)
-    const [eventosBiberonHoy, setEventosBiberonHoy] = useState([])
-    const [ultimoEventoBiberon, setUltimoEventoBiberon] = useState(null)
     const [horaUltimoBiberonHoy, setHoraUltimoBiberonHoy] = useState('')
     const [cronometro, setCronometro] = useState(0)
-
     let contador = 0;
-    // Calcular cuantos biberon fueron tomados el dia de hoy
+    // Calcular cuantos biberones fueron tomados el dia de hoy
     eventos.map(e => {
         if (e.idCategoria == 35 && e.fecha.split(' ')[0] === new Date().toISOString().split('T')[0]) {
-            contador++;
+            contador++
         }
     })
 
@@ -22,22 +19,30 @@ const BiberonesConsumidos = () => {
 
             // Obtengo todos los eventos de biberon del dia de hoy y asigno el valor al estado
             let eventosBiberon = eventos.filter(e => e.idCategoria == 35 && e.fecha.split(' ')[0] == new Date().toISOString().split('T')[0])
-            setEventosBiberonHoy(eventosBiberonHoy)
+
 
             // Obtengo el ultimo evento biberon del dia de hoy de los eventos biberones de hoy
             let ultimoEvento = eventosBiberon[eventosBiberon.length - 1]
-            setUltimoEventoBiberon(ultimoEvento)
+
 
             // Obtengo la hora del ultimo biberon consumido
             let horaUltimoBiberon = ultimoEvento.fecha.split(' ')[1]
             setHoraUltimoBiberonHoy(horaUltimoBiberon)
+            calcularCronometro()
         } else {
             setHoraUltimoBiberonHoy('')
+            setCronometro(0)
         }
     }, [eventos])
 
-    const calcularCronometro = () => {        
-        
+    const calcularCronometro = () => {
+        let horaActual = new Date().toISOString().split('T')[1].split('.')[0]
+        console.log(horaActual)
+        console.log(horaUltimoBiberonHoy)
+        const diferenciaHoras = horaActual.split(':')[0] - horaUltimoBiberonHoy.split(':')[0]
+        const diferenciaMinutos = horaActual.split(':')[1] - horaUltimoBiberonHoy.split(':')[1]
+        const diferenciaTotal = `${diferenciaHoras - 3} hora/s ${diferenciaMinutos} minuto/s`
+        setCronometro(diferenciaTotal)
     }
 
 
@@ -47,7 +52,7 @@ const BiberonesConsumidos = () => {
         <div>
             <h3>Biberones consumidos hoy:</h3>
             <p>{contador}</p>
-            <h3>Ultimo biberón consumido:</h3>
+            <h3>Ultimo biberón consumido hace:</h3>
             <p>{cronometro}</p>
         </div>
     )
