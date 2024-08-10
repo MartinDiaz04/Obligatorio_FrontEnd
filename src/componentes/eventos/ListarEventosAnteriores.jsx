57501b99a0a3ecc5b2b9b80f48dfcb29e76015c9
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-10 14:46:51 -0300

    refactor: Add ListarEventosAnteriores component to display previous events

---
+++ src/componentes/eventos/ListarEventosAnteriores.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux"
+import { useState, useEffect } from "react"
+import Evento from "./Evento"
+
+const ListarEventos = () => {
+  const eventos = useSelector(state => state.evento.listaEventos)  
+  const hoy = new Date()
+  return (
+    <div className="row d-flex justify-content-center listaEventos">
+      
+        <h1 className="text-center">Eventos totales</h1>
+        {/* Le saco la hora para comprar solo el dia */}
+        {eventos.filter(e => e.fecha.split(' ')[0] < hoy.toISOString().split('T')[0]).map(e => (
+          <Evento key={e.id} {...e} />
+        ))
+        }
+
+    </div>
+
+  )
+}
+
+export default ListarEventos
