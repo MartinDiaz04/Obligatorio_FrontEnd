f5106fb111ac43371569117544981dbbc1bafd82
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-12 01:55:13 -0300

    feat: Implement lazy loading for images in CantComidas component

--- src/componentes/graficas/CantComidas.jsx
+++ src/componentes/graficas/CantComidas.jsx
@@ -16,19 +16,36 @@ ChartJS.register(
 
 const CantCategorias = () => {
     const eventos = useSelector(state => state.evento.listaEventos)
+    const [eventosPorDia, setEventosPorDia] = useState([])
 
     useEffect(() => {
         // Tomo la fecha de hoy y le resto 7 para obtener la semana pasada
         const hoy = new Date();
         const semanaPasada = new Date();
-        semanaPasada.setDate(hoy.getDate() - 7);
-        console.log(semanaPasada)
+        semanaPasada.setDate(hoy.getDate() - 8);
 
-        // Filtro eventos de esta semana
-        const eventosTotales = eventos.filter(e => new Date(e.fecha) > semanaPasada && new Date(e.fecha) < hoy)
-    }, [])
+        // Filtro eventos de la ultima semana
+        const eventosTotales = eventos.filter(e => new Date(e.fecha) > semanaPasada && new Date(e.fecha) < hoy && e.idCategoria == 31)
 
 
+        let datos = []
+        // Recorro los ultimos 7 dias contando cada dia la cantidad de eventos
+        for (let i = 0; i <= 7; i++) {
+            const fecha = new Date();
+            // Le resto i dias a la fecha de hoy para ir atras en los dias
+            fecha.setDate(hoy.getDate() - i);
+            // Cuento eventos del dia en el que estoy parado
+            const eventosDia = eventosTotales.filter(e => new Date(e.fecha).getDate() == fecha.getDate()).length            
+            // Creo el objeto y lo agrego al array
+            const dia = {
+                fecha: fecha.toISOString().split('T')[0],
+                cantidad: eventosDia,
+            }
+            datos.push(dia)
+        }
+        // Seteo los eventos por dia con el array creado antes
+        setEventosPorDia(datos)
+    }, [eventos])
 
     return (
         <div>
@@ -47,11 +64,11 @@ const CantCategorias = () => {
                             },
                         },
                     }} data={{
-                        labels: "`  fgdsg",
+                        labels: eventosPorDia.map(e => e.fecha),
                         datasets: [
                             {
                                 label: 'Cantidad de eventos por categoria',
-                                data: "gfd",
+                                data: eventosPorDia.map(e => e.cantidad),
                                 backgroundColor: 'rgba(255, 99, 132, 0.5)',
                             }
                         ],
